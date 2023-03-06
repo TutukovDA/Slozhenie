d2f035cc812e3a54886baccfd53e3bdc4551b774
TutukovDA <126241028+TutukovDA@users.noreply.github.com>
2023-03-06 23:32:43 +0400

    Напишите программу на Java для вывода суммы двух чисел. Перейти к редактору Тестовые данные: 74 + 36 Ожидаемый результат : 110

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+                int x = 74;
+                int y = 36;
+               System.out.println(x+y);
+    }
+}
